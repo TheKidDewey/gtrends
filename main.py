ca1c48c920e682c7520f4c51842b2e6a1a9e483d
Alex <alexdewey91@hotmail.co.uk>
2021-09-28 10:41:23 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from pytrends.request import TrendReq
+import pandas as pd
+import time
+
+startTime = time.time()
+pytrend = TrendReq(hl='en-GB', tz=360)
+
+colnames = ["keywords"]
+df = pd.read_csv("keyword_list.csv", names=colnames)
+df2 = df["keywords"].values.tolist()
+df2.remove("Keywords")
+
+dataset = []
+
+for x in range(0,len(df2)):
+     keywords = [df2[x]]
+     pytrend.build_payload(
+     kw_list=keywords,
+     cat=0,
+     timeframe='now 7-d',
+     geo='US')
+     data = pytrend.interest_over_time()
+     if not data.empty:
+          data = data.drop(labels=['isPartial'],axis='columns')
+          dataset.append(data)
+
+result = pd.concat(dataset, axis=1)
+result.to_csv('trends.csv')
+
+executionTime = (time.time() - startTime)
+print('Execution time in sec.: ' + str(executionTime))
